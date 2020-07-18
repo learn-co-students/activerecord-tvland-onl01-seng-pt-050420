e98a0386243707a6b2f6f45b63b6d370750d314d
C0mbed <matt@farncombe.co>
2020-07-18 13:46:07 -0600

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    belongs_to :network
+
+    def actors_list
+        actor_id_list = self.characters.collect {|c| c.actor_id }
+        actor_list = []
+        actor_id_list.each do |actor_id|
+            actor = Actor.find_by(id: actor_id)
+            actor_list << actor.full_name
+        end
+        actor_list
+    end
   
 end
