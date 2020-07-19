7970ee234160c5e47c3ef149537ffdbbea642b5e
ValentinaPanic <valentina.panic87@gmail.com>
2020-07-19 13:19:31 -0600

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,11 @@
 class Show < ActiveRecord::Base
     has_many :characters
     belongs_to :network
+
+    def actors_list
+        self.characters.collect do |a|
+            "#{a.actor.first_name} #{a.actor.last_name}"
+            # binding.pry
+        end
+    end
 end
