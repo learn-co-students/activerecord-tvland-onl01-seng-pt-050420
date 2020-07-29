d60a3ea20f5c69e7acd603ce9b69307a6bb59d88
tpham13 <tpham0123@gmail.com>
2020-07-29 14:46:38 -0700

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,10 +1,11 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    belongs_to :network 
     has_many :actors, through: :characters
-    has_many :characters 
-    belongs_to :network
+    
 
     def actors_list
-        self.actors.map{|actor| actor.full_name}
+        self.actors.map {|actor| actor.full_name}
     end 
     
   
