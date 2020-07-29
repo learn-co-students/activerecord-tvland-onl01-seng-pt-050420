dff6934eaae10dd3630fe25a3b46cdd07ca1eb07
tpham13 <tpham0123@gmail.com>
2020-07-29 13:48:42 -0700

    Done.

---
+++ db/migrate/005_create_characters.rb
@@ -0,0 +1,10 @@
+class CreateCharacters < ActiveRecord::Migration[5.2]
+    def change
+        create_table :characters do |t|
+            t.string name
+            t.integer :actor_id     #a character will belong to a show (the show migration is already provided) and an actor, and we'll keep track of this relationship with these database table columns.
+            t.integer :show_id
+        end
+    end
+end
+
